276828ae7271987be30a85e8c61741727f4af724
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2024-04-21 16:31:18 +0200

    cambios

--- ProyectoFinal/Assets/AtaqueDefensa.cs
+++ ProyectoFinal/Assets/AtaqueDefensa.cs
@@ -9,14 +9,10 @@ public class AtaqueDefensa : VisualElement
     VisualElement atq1 = new VisualElement();
     VisualElement atq2 = new VisualElement();
     VisualElement atq3 = new VisualElement();
-    VisualElement atq4 = new VisualElement();
-    VisualElement atq5 = new VisualElement();
 
     VisualElement def1 = new VisualElement();
     VisualElement def2 = new VisualElement();
     VisualElement def3 = new VisualElement();
-    VisualElement def4 = new VisualElement();
-    VisualElement def5 = new VisualElement();
 
     int atqEstado;
     public int AtqEstado
@@ -48,10 +44,6 @@ public class AtaqueDefensa : VisualElement
         atq2.style.opacity = 0.5f;
         atq3.style.backgroundImage = new StyleBackground(image_atq);
         atq3.style.opacity = 0.5f;
-        atq4.style.backgroundImage = new StyleBackground(image_atq);
-        atq4.style.opacity = 0.5f;
-        atq5.style.backgroundImage = new StyleBackground(image_atq);
-        atq5.style.opacity = 0.5f;
 
         def1.style.backgroundImage = new StyleBackground(image_def);
         def1.style.opacity = 0.5f;
@@ -59,22 +51,14 @@ public class AtaqueDefensa : VisualElement
         def2.style.opacity = 0.5f;
         def3.style.backgroundImage = new StyleBackground(image_def);
         def3.style.opacity = 0.5f;
-        def4.style.backgroundImage = new StyleBackground(image_def);
-        def4.style.opacity = 0.5f;
-        def5.style.backgroundImage = new StyleBackground(image_def);
-        def5.style.opacity = 0.5f;
 
         if (atqEstado > 0) { atq1.style.opacity = 1f; }
         if (atqEstado > 1) { atq2.style.opacity = 1f; }
         if (atqEstado > 2) { atq3.style.opacity = 1f; }
-        if (atqEstado > 3) { atq4.style.opacity = 1f; }
-        if (atqEstado > 4) { atq5.style.opacity = 1f; }
 
         if (defEstado > 0) { def1.style.opacity = 1f; }
         if (defEstado > 1) { def2.style.opacity = 1f; }
         if (defEstado > 2) { def3.style.opacity = 1f; }
-        if (defEstado > 3) { def4.style.opacity = 1f; }
-        if (defEstado > 4) { def5.style.opacity = 1f; }
 
     }
 
@@ -99,46 +83,30 @@ public class AtaqueDefensa : VisualElement
         }
     };
     public AtaqueDefensa() {
-        atq1.style.width = 50;
-        atq1.style.height = 50;
+        atq1.style.width = 30;
+        atq1.style.height = 30;
 
-        atq2.style.width = 50;
-        atq2.style.height = 50;
+        atq2.style.width = 30;
+        atq2.style.height = 30;
 
-        atq3.style.width = 50;
-        atq3.style.height = 50;
+        atq3.style.width = 30;
+        atq3.style.height = 30;
 
-        atq4.style.width = 50;
-        atq4.style.height = 50;
+        def1.style.width = 30;
+        def1.style.height = 30;
 
-        atq5.style.width = 50;
-        atq5.style.height = 50;
+        def2.style.width = 30;
+        def2.style.height = 30;
 
-        def1.style.width = 50;
-        def1.style.height = 50;
-
-        def2.style.width = 50;
-        def2.style.height = 50;
-
-        def3.style.width = 50;
-        def3.style.height = 50;
-
-        def4.style.width = 50;
-        def4.style.height = 50;
-
-        def5.style.width = 50;
-        def5.style.height = 50;
+        def3.style.width = 30;
+        def3.style.height = 30;
 
         hierarchy.Add(atq1);
         hierarchy.Add(atq2);
         hierarchy.Add(atq3);
-        hierarchy.Add(atq4);
-        hierarchy.Add(atq5);
 
         hierarchy.Add(def1);
         hierarchy.Add(def2);
         hierarchy.Add(def3);
-        hierarchy.Add(def4);
-        hierarchy.Add(def5);
     }
 }
